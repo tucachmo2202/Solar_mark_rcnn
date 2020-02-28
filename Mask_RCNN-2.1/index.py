f70561248a773523d0d50f3b00f388381e75fb20
tucachmo2202 <47667644+tucachmo2202@users.noreply.github.com>
2020-02-28 09:43:25 +0700

    Add reject_line() function to check overlap line solar

--- Mask_RCNN-2.1/index.py
+++ Mask_RCNN-2.1/index.py
@@ -1,20 +1,32 @@
 import cv2
 import json
 import os
-import numpy as np 
+import numpy as np
 from PIL import Image
 
 
+#Tham so cho ham HoughLinesP
+RHO = 10 #The resolution of the parameter r in pixels.
+THRESHOLD = 5 #The minimum number of intersections to “detect” a line
+MINLINLENGTH = 450 #The minimum number of points that can form a line.
+MAXLINEGAP = 280 #The maximum gap between two points to be considered in the same line.
+
 class Line():
-    def __init__(self, index, start, end, points = []):
+
+    def __init__(self, index = 0, start = [0, 0], end = [0, 0], points = []):
         self.index = index
         self.start = start
         self.end = end
-        self.points = points
+        self.points = list(points)
     
+    def set_line(self, index, start, end):
+        self.index = index
+        self.start = start
+        self.end = end
+
     def update(self, point):
         self.points.append(point)
-    
+
     def get_line(self):
         return self.index, self.start, self.end
 
@@ -28,11 +40,20 @@ def distance(start, end, point):
     point = np.asarray(point)
     return abs(np.cross(end-start, point - start)/np.linalg.norm(end-start))
 
+#Hàm này loại bỏ những đường thẳng song song trùng nhau
+def reject_line(list_line):
+    for j in range(len(list_line) - 1, -1, -1):
+        for i in range(len(list_line) - 1, -1, -1):
+            x1,y1 = list_line[j][:2]
+            if (i!=j and distance((list_line[i][0], list_line[i][1]), (list_line[i][2], list_line[i][3]), (x1, y1)) < 5):
+                del list_line[i]
+                print("1 line removed")
+                break
+    return list_line
+
 def index(img, bounding_boxs, rect):
     #Create mask:
     w, h = img.shape[0:2]
-    print("weight", w)
-    print("height", h)
     mask = np.zeros((w, h))
     bounding_boxs_center = []
     for bb in bounding_boxs:
@@ -52,9 +73,10 @@ def index(img, bounding_boxs, rect):
     gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
     # Find the edges in the image using canny detector
     edges = cv2.Canny(gray, 50, 200)
+    #88 image is 5, 10, 700, 1024
     # Detect points that form a line
     max_slider = 5 #Số lượng điểm giao nhau tối thiểu để tạo thành đường thẳng
-    lines = cv2.HoughLinesP(edges, 10, np.pi/180, max_slider, minLineLength=700, maxLineGap = 1024)
+    lines = cv2.HoughLinesP(edges, RHO, np.pi/180, THRESHOLD, minLineLength = MINLINLENGTH, maxLineGap = MAXLINEGAP)
     # Draw lines on the image
     print(len(lines))
     lines = [line[0] for line in lines]
@@ -62,49 +84,63 @@ def index(img, bounding_boxs, rect):
     i = 0
     #Sắp xếp các line
     lines.sort(key = lambda lines: lines[1])
+    lines = reject_line(lines)
     for line in lines:
-        print(line)
         x1, y1, x2, y2 = line
-        line_object = Line(i, [x1, y1], [x2, y2])
+        start = [x1, y1]
+        end = [x2, y2]
+        line_object = Line(i, start, end)
         line_array.append(line_object)
-        cv2.putText(img, str(i), (x1, y1), cv2.FONT_HERSHEY_PLAIN, 1, (25,112,3), 1)
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
         i+=1
-    # Show result
-    # for bb in bbs:
-    #     cv2.rectangle(img, (bb[1], bb[0]), (bb[3], bb[2]), (55, 255, 0), 1)
+
 
     for box in rect:
         box = np.int0(box)
-        cv2.drawContours(img, [box], 0, (0,0,255), 1)
+        cv2.drawContours(img, [box], 0, (255,255,255), 1)
 
     cv2.imshow("Result Image", img)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
     threshold = 8
-    print(len(bounding_boxs_center))
     bounding_boxs_center.sort(key = lambda bounding_boxs_center: bounding_boxs_center[0])
+    
     for line in line_array:
         index, start, end = line.get_line()
         i = 0
         for center_point in bounding_boxs_center:
-            if (distance(start, end, center_point) < threshold):
+            cv2.circle(img, tuple(center_point), 3, (0,0,255), -1)
+            # print(distance(start, end, tuple(center_point)))
+            if (distance(start, end, tuple(center_point)) <= 7.0):
                 line.update(center_point)
-                cv2.putText(img, str(index) + "-" + str(i), tuple(center_point), cv2.FONT_HERSHEY_PLAIN, 1, (25,112,3), 1)
+                # cv2.putText(img, str(index) + "-" + str(i), tuple(center_point), cv2.FONT_HERSHEY_PLAIN, 1, (25,112,3), 1)
                 i += 1
-                print(str(i))
 
+    #Sắp xếp line theo giá trị y của điểm đầu
+    line_array = sorted(line_array, key=lambda x: x.points[1])
+   
+    #Hiển thị lên ảnh
+    i = 0
+    for line_object in line_array:
+        x1, y1 = line_object.start
+        x2, y2 = line_object.end
+        cv2.putText(img, str(i), (x1, y1), cv2.FONT_HERSHEY_PLAIN, 1, (25,112,3), 1)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 1)
+        j = 0
+        for point in line_object.points:
+            cv2.putText(img, str(i) + "-" + str(j), tuple(point), cv2.FONT_HERSHEY_PLAIN, 1, (25,112,3), 1)
+            j += 1
+        i += 1
     cv2.imshow("Result Image", img)
     cv2.waitKey()
     cv2.destroyAllWindows()
+    cv2.imwrite("/home/manhas/Desktop/ressd.jpg", img)
 
 res = json.load(open("/home/manhas/Desktop/result_image/save_folder/84.json"))
 bbs = res["bounding_box"]
 rect = res["boxes"]
-print(rect)
 
-img = cv2.imread("/home/manhas/Desktop/result_image/save_folder/84.jpg")
+img = cv2.imread("/home/manhas/Desktop/84.jpg")
 cv2.imshow("sss", img)
 cv2.waitKey()
 cv2.destroyAllWindows()
